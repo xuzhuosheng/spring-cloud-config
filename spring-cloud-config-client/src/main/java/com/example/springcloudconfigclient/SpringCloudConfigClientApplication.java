3d56723373df778b7b665cd0f2c5177e95b3e129
xuzhuosheng <284696920@qq.com>
2019-07-08 17:35:44 +0800

    spring-cloud-config-client

---
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -0,0 +1,28 @@
+package com.example.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
+public class SpringCloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
+
+    @Value ("${name}")
+    private String name;
+
+    @RequestMapping ("/test")
+    public String test() {
+        return this.name;
+    }
+}
